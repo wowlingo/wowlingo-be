2719508cb0ef168d02202971d4632b4427bc0c0a
Yejin Jang <jyjin0428@gmail.com>
2025-11-30 23:15:35 +0900

    update: 환경 변수 설정 및 dotenv 초기화 추가

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import * as express from 'express';
+import { config } from 'dotenv';
+
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
